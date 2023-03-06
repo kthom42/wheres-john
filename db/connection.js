d55a4e8f07fd032d63d6f69fdda92da36200f0ae
kthom42 <kmgt42@gmail.com>
2023-03-05 22:13:40 -0500

    changed port and added routes for prompts

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const connection = mysql.createConnection({
   host: "localhost",
-  port: 3001,
+  port: 3306,
   user: "root",
   password: process.env.MYSQL_PASSWORD,
   database: "employees",
